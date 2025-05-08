ddc7d9132a9501fa81e3956d8213ead51f37c730
dndjeh <dndjeh12@naver.com>
2025-05-08 21:24:12 +0900

    Chap2: Aapah spark start

--- chapter2/mmcount.py
+++ chapter2/mmcount.py
@@ -1,9 +1,8 @@
 # 예제 2-1 M&M 개수 집계
+#실행 방법 : python mmcount.py 데이터셋 경로 
 
 import sys
 from pyspark.sql import SparkSession
-
-#python mmcount.py 데이터셋 절대 경로  #실행 방법
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
